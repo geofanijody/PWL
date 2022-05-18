0ee4039e81331333c2c8615729684641da9b423a
geofanijody <101728882+geofanijody@users.noreply.github.com>
2022-05-18 21:38:36 +0700

    Add files via upload

---
+++ Tb User_A11.2018.10915.php
@@ -0,0 +1,30 @@
+<?php
+$username = "username";
+$password = "password";
+$name   = "nama";
+$hak = "hak";
+
+// Create connection
+$conn = mysqli_connect($username, $password, $nama, $hak);
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+// sql to create table
+$sql = "CREATE TABLE MyGuests (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+firstname VARCHAR(30) NOT NULL,
+lastname VARCHAR(30) NOT NULL,
+email VARCHAR(50),
+reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+)";
+
+if (mysqli_query($conn, $sql)) {
+  echo "Table MyGuests created successfully";
+} else {
+  echo "Error creating table: " . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+?>
